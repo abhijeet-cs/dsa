97c3de52d187375f5f2df4c9f169b6cf3b2389bf
Abhijeet Bhattacharya <abhijeetbhattacharya.learnings@gmail.com>
2026-04-06 21:12:29 +0530

    Create PopularBS.java

---
+++ bs/PopularBS.java
@@ -0,0 +1,30 @@
+public class BS {
+    public static int bs(int[] a, int t) {
+        int l = 0, r = a.length - 1;
+        while (l <= r) {
+            int m = (l + r) >>> 1;
+            if (a[m] == t) return m;
+            if (a[m] > t) r = m;
+            else l = m + 1;
+        }
+        return -1;
+    }
+    public static int peak(int[] a) {
+        int l = 0, h = a.length - 1;
+        while (l < h) {
+            int m = (l + h) >>> 1;
+            if (a[m + 1] > a[m]) l = m + 1;
+            else h = m;
+        }
+        return l;
+    }
+    public static int pivot(int[] a) {
+        int l = 0, h = a.length - 1;
+        while (l < h) {
+            int m = (l + h) >>> 1;
+            if (a[m] > a[h]) l = m + 1;
+            else h = m;
+        }
+        return l;
+    }
+}
